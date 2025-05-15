e3fbbbbec1741f201a9f6d8b86aecae374f9b6f8
Winn4K <kauan99436601@gmail.com>
2025-05-15 18:19:20 -0300

    ajustes de caminhos

--- src/views/AcoesSociais/acoesSociais.jsx
+++ src/views/AcoesSociais/acoesSociais.jsx
@@ -1,5 +1,5 @@
-import SoldadinhosDeCristo from "../../components/SoldadinhosDeCristo/SoldadinhosDeCristo";
-import ResumoDoacoes from "../../components/ResumoDoacoes/ResumoDoacoes";
+import SoldadinhosDeCristo from "../../components/SoldadinhosDeCristo/soldadinhosDeCristo";
+import ResumoDoacoes from "../../components/ResumoDoacoes/resumoDoacoes";
 import NatalSolidario from "../../components/NatalSolidario/natalSolidario";
 import './index.scss';
 
